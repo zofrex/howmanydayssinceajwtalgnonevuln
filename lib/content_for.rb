dbcb5569dc07e1f51e5050777115d37ec8305bec
James 'zofrex' Sanderson <zofrex@gmail.com>
2020-10-03 11:04:19 +0100

    Improve ergonomics of using content for with if_content_for block

--- lib/content_for.rb
+++ lib/content_for.rb
@@ -1,3 +1,5 @@
+require 'cgi'
+
 class ContentFor < Liquid::Block
   SYNTAX_HELP = "Syntax Error in tag 'content_for' - Valid syntax: {% content_for name %}".freeze
 
@@ -27,8 +29,37 @@ end
 Liquid::Template.register_tag('content_for', ContentFor)
 
 class YieldContent < Liquid::Tag
-  SYNTAX_HELP = "Syntax Error in tag 'yield_content' - Valid syntax: {% yield_content name %}".freeze
+  SYNTAX_HELP = "Syntax Error in tag 'yield_content' - Valid syntax: {% yield_content name [escape] %}".freeze
 
+  def initialize(tag_name, markup, parse_context)
+    params = markup.split(/\s+/)
+    raise SyntaxError, SYNTAX_HELP unless params.length == 1 || params.length == 2
+    @name = params.first
+    @escape = false
+    if params[1]
+      if params[1] == 'escape'
+        @escape = true
+      else
+        raise SyntaxError, SYNTAX_HELP
+      end
+    end
+    super(tag_name, markup, parse_context)
+  end
+
+  def render(context)
+    content_for = context.environments.first['middleman'].instance_variable_get('@content_for')
+    content = content_for[@name] || ''
+    if @escape
+      CGI.escapeHTML(content)
+    else
+      content
+    end
+  end
+end
+
+Liquid::Template.register_tag('yield_content', YieldContent)
+
+class IfContentFor < Liquid::Block
   def initialize(tag_name, markup, parse_context)
     params = markup.split(/\s+/)
     raise SyntaxError, SYNTAX_HELP unless params.length == 1
@@ -38,8 +69,16 @@ class YieldContent < Liquid::Tag
 
   def render(context)
     content_for = context.environments.first['middleman'].instance_variable_get('@content_for')
-    content_for[@name] || ''
+    if content_for&.dig(@name)
+      @body.render(context)
+    else
+      ''
+    end
+  end
+
+  def block_delimiter
+    "end_#{block_name}"
   end
 end
 
-Liquid::Template.register_tag('yield_content', YieldContent)
+Liquid::Template.register_tag('if_content_for', IfContentFor)
